eafe2c41ed3d0aebede05f9abfb27e727e27cd47
Kevin Tovar <kevintovar@esimez.mx>
2020-08-07 16:50:40 -0500

    Add route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const Home = lazy(() => import('./screens/Home'))
+const Login = lazy(() => import('./screens/Login'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={<></>}>
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Router>
+    </Suspense>
+  )
 }
 
-export default App;
+export default App
